05f33d3865bf13235c87f3f69779820180b92a41
damian <dfauth@gmail.com>
2026-02-18 15:03:07 +0800

    refactor ExponentialMovingAverage

--- ta/src/main/java/io/github/dfauth/ta/ExponentialMovingAverage.java
+++ ta/src/main/java/io/github/dfauth/ta/ExponentialMovingAverage.java
@@ -3,37 +3,45 @@ package io.github.dfauth.ta;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.BinaryOperator;
 import java.util.function.Function;
 
+import static io.github.dfauth.ta.SimpleMovingAverage.sma;
 import static java.util.Optional.empty;
 
 public class ExponentialMovingAverage {
 
     private ExponentialMovingAverage() {}
 
+    public static BinaryOperator<Double> ema(double smoothingFactor, int period) {
+        double multiplier = smoothingFactor / (period + 1);
+        return (current, previous) -> (current * multiplier) + (previous * (1 - multiplier));
+    }
+
+    public static double ema(double[] prices) {
+        return ema(prices, prices.length)[0];
+    }
+
     public static double[] ema(double[] prices, int period) {
+        return ema(2.0, prices, period);
+    }
+
+    public static double[] ema(double smoothingFactor, double[] prices, int period) {
         if (period < 1) {
             throw new IllegalArgumentException("Period must be at least 1");
         }
         if (prices.length < period) {
             return new double[0];
         }
-        double multiplier = 2.0 / (period + 1);
         int resultLength = prices.length - period + 1;
         double[] result = new double[resultLength];
-        double sum = 0;
-        for (int i = 0; i < period; i++) {
-            sum += prices[i];
-        }
-        result[0] = sum / period;
+        result[0] = sma(prices, period)[0];
+        double prev = result[0];
         for (int i = 1; i < resultLength; i++) {
-            result[i] = (prices[i + period - 1] - result[i - 1]) * multiplier + result[i - 1];
+            result[i] = ema(smoothingFactor, period).apply(prices[resultLength+i-1], prev);
+            prev = result[i];
         }
         return result;
-    }
-
-    public static double ema(double[] prices) {
-        return ema(prices, prices.length)[0];
     }
 
     public static Function<Double, Optional<Double>> ema(int period) {
